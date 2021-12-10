aa6bd666085017dd436a784e108a5428ebe80a1c
Maxime Gauthier <max45270@gmail.com>
2021-12-10 14:43:37 +0100

    init

---
+++ assets/components/DemoComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h1>Ceci est notre composant 1 de démo !</h1>
+        <a href="https://www.maxime-gauthier.com" target="_blank">Venez visiter mon site</a>
+    </div>
+</template>
+
+<script type="application/javascript">
+export default {
+  name: "DemoComponent"
+};
+</script>
+
+<style scoped>
+h1 {
+    background-color: red;
+}
+</style>
---
+++ assets/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Hello !</div>
+  <demo-component />
+</template>
+
+<script>
+import DemoComponent from '../components/DemoComponent.vue';
+export default {
+  components: {
+    DemoComponent
+  }
+}
+</script>
+
+<style>
+</style>
