22d7364814dc376358be5087c77c4b8103fa9e9b
Maxime Gauthier <max45270@gmail.com>
2021-12-10 20:42:13 +0100

    dateTimeRdy

--- assets/components/topbar.vue
+++ assets/components/topbar.vue
@@ -1,14 +1,42 @@
 <template>
     <nav>
         <div>Activities</div>
-        <div>déc.10 15:24</div>
+        <div>{{ nowTime }}</div>
         <div>Settings</div>
     </nav>
 </template>
 
 <script type="application/javascript">
 export default {
-  name: "topbar"
+    name: "topbar",
+    data () {
+        return {
+        nowTime: null,
+        nowTimeStamp: null,
+        tsFormatter: Intl.DateTimeFormat('fr', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' })
+        }
+    },
+    methods: {
+        formatTimestamp (ts) {
+            // console.log(ts);
+            return this.tsFormatter.format(ts);
+        },
+        dateNow () {
+            this.nowTimeStamp = Date.now();
+            setInterval (() => {
+                this.nowTimeStamp = Date.now();
+            }, 1000)
+        },
+    },    
+    watch: {
+        nowTimeStamp (newVal, oldVal) {
+            // console.log(newVal, oldVal);
+            this.nowTime = this.formatTimestamp(newVal);
+        },
+    },
+    beforeMount(){
+        this.dateNow();
+    }
 };
 </script>
 
