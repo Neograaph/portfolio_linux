a75e36fd021007d3a94fa144ff6907161aa89c68
Maxime Gauthier <max45270@gmail.com>
2021-12-13 11:54:39 +0100

    portfolio

--- assets/components/portfolio.vue
+++ assets/components/portfolio.vue
@@ -15,8 +15,8 @@
         </svg>
     </div>
     <div class="noteContent">
-      <div class="containerCards">
-        <div v-for='project in this.portfolio' :key='project' class="card">
+      <!-- <div class="containerCards"> -->
+        <!-- <div v-for='project in this.portfolio' :key='project' class="card">
             <div class="nameProject">{{project.name}}</div>
             <div class="imgProject"><img :src="project.img" alt="image non disponible"> </div>
             <div>{{project.languages}}</div>
@@ -24,8 +24,24 @@
             <div><a :href="project.link_web" target="_blank"></a></div>
             <div>{{project.link_repo}}</div>
             <div>{{project.date}}</div>
-        </div>
-      </div>
+        </div> -->
+
+            <div class="wrap" >
+              <div class="tile" v-for='project in this.portfolio' :key='project'> 
+                <img :src="project.img" alt="image non disponible"/>
+                <div class="text">
+                <h1>{{project.name}}</h1>
+                <h2 class="animate-text">{{project.languages}}</h2>
+                <p class="animate-text">{{project.description}}</p>
+              <div class="dots">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+                </div>
+              </div>
+            </div>
+      <!-- </div> -->
     </div>
     <div class="noteFooter">
       <div>loading file "/home/maxime/desktop/portfolio"...</div>
@@ -153,6 +169,9 @@ export default {
     overflow-y: scroll;
     scrollbar-color: rebeccapurple green;
     scrollbar-width: thin;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
   .noteFooter{
     background: #292825;
@@ -162,32 +181,178 @@ export default {
     height: 30px;
     padding: 0 10px;
   }
-  .containerCards{
+  /* .containerCards{
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-  }
-  .card{
+  } */
+  /* .card{
     margin: 1%;
     border: solid 1px black;
     height: 35vh;
     width: 25vw;
     /* min-height: 40vh;
-    min-width: 20vw; */
+    min-width: 20vw;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
     padding: 5px;
-  }
-  .card img{
+  } */
+  /* .card img{
     max-height: 20vh;
     max-width: 20vw;
-  }
-  .imgProject{
+  } */
+  /* .imgProject{
     flex-grow: 10;
   }
   .nameProject{
     margin-bottom: 10px;
+  } */
+
+
+
+  .wrap
+{
+  margin:50px auto 50px auto;
+  width:100%;
+  display:flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  /* max-width:1200px; */
+  row-gap: 5rem;
+}
+.tile
+{
+  width:380px;
+  height:380px;
+  margin:10px;
+  background-color:#99aeff;
+  display:inline-block;
+  background-size:cover;
+  position:relative;
+  cursor:pointer;
+  transition: all 0.4s ease-out;
+  box-shadow: 0px 35px 77px -17px rgba(0,0,0,0.44);
+  overflow:hidden;
+  color:white;
+  font-family:'Roboto';
+  width: 400px;
+  
+}
+.tile img
+{
+  height:100%;
+  width:100%;
+  position:absolute;
+  top:0;
+  left:0;
+  z-index:0;
+  transition: all 0.4s ease-out;
+}
+.tile .text
+{
+/*   z-index:99; */
+  position:absolute;
+  padding:30px;
+  height:calc(100% - 60px);
+}
+.tile h1
+{
+ 
+  font-weight:300;
+  margin:0;
+  text-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+}
+.tile h2
+{
+  font-weight:100;
+  margin:20px 0 0 0;
+  font-style:italic;
+   transform: translateX(200px);
+}
+.tile p
+{
+  font-weight:300;
+  margin:20px 0 0 0;
+  line-height: 25px;
+/*   opacity:0; */
+  transform: translateX(-200px);
+  transition-delay: 0.2s;
+}
+.animate-text
+{
+  opacity:0;
+  transition: all 0.6s ease-in-out;
+}
+.tile:hover
+{
+/*   background-color:#99aeff; */
+box-shadow: 0px 35px 77px -17px rgba(0,0,0,0.64);
+  transform:scale(1.05);
+}
+.tile:hover img
+{
+  opacity: 0.2;
+}
+.tile:hover .animate-text
+{
+  transform:translateX(0);
+  opacity:1;
+}
+.dots
+{
+  position:absolute;
+  bottom:20px;
+  right:30px;
+  margin: 0 auto;
+  width:30px;
+  height:30px;
+  color:currentColor;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:space-around;
+  
+}
+
+.dots span
+{
+    width: 5px;
+    height:5px;
+    background-color: currentColor;
+    border-radius: 50%;
+    display:block;
+  opacity:0;
+  transition: transform 0.4s ease-out, opacity 0.5s ease;
+  transform: translateY(30px);
+ 
+}
+
+.tile:hover span
+{
+  opacity:1;
+  transform:translateY(0px);
+}
+
+.dots span:nth-child(1)
+{
+   transition-delay: 0.05s;
+}
+.dots span:nth-child(2)
+{
+   transition-delay: 0.1s;
+}
+.dots span:nth-child(3)
+{
+   transition-delay: 0.15s;
+}
+
+
+@media (max-width: 1000px) {
+  .wrap {
+   flex-direction: column;
+    width:400px;
   }
+}
 </style>
