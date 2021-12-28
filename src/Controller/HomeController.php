c309c370d6d7bd94f7bb90b1cc6d22acb9e3ceb5
Maxime Gauthier <max45270@gmail.com>
2021-12-28 13:25:33 +0100

    bug meta

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('basevue.html.twig');
     }
     /**
      * @Route("/api/portfolio", name="portfolio")
