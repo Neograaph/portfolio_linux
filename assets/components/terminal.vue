69efdbfa40d5dd23ad5ad395b9742f89fffa02e6
Maxime Gauthier <max45270@gmail.com>
2021-12-30 00:57:24 +0100

    responsive terminal

--- assets/components/terminal.vue
+++ assets/components/terminal.vue
@@ -5,22 +5,53 @@
   </figure>
 
   <div v-if='pageOpen' class="page">
-    <div class="noteHeader">
+
+    <!-- popup terminal -->
+    <div class="page1">  
+      <div class="noteHeader">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 svgTop" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+          </svg>
+          Terminal
+          <svg @click="closePage" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 closeBtn svgTop" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+      </div>
+      <div class="noteContent">
+        <form class="formField" action="javascript:void(0)">
+        maxime@maxime-HM863BX ~ $
+        <input class="inputField" type="text">
+        <input class="submitField" type="submit" value=">">
+        </form>
+      </div>
+    </div>
+    <!-- fin popup terminal -->
+
+    <!-- début popup notes -->
+    <div class="page2">
+      <div class="noteHeader">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 svgTop" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
         </svg>
-        Terminal
+        Commandes_terminal.txt
         <svg @click="closePage" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 closeBtn svgTop" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
+      </div>
+      <div class="noteContent">
+        - inscription <br/>
+        - connexion <br/>
+        - download (pour télécharger mon CV) <br/>
+      </div>
+      <div class="noteFooter">
+        <div>loading /notes.txt"...</div>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 svgBot" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+        </svg>
+      </div>
     </div>
-    <div class="noteContent">
-      <form class="formField" action="javascript:void(0)">
-      maxime@maxime-HM863BX ~ $
-      <input class="inputField" type="text">
-      <input class="submitField" type="submit" value=">">
-      </form>
-    </div>
+    <!-- fin popup notes -->
+
   </div>
 </template>
 
@@ -85,19 +116,19 @@ export default {
   .page{
     position: absolute;
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
+    /* flex-direction: column; */
+    justify-content: space-evenly;
     margin-left: auto;
     margin-right: auto;
-    top: 150px;
+    top: 60px;
     left: 0;
     right: 0;
-    background: RGB(48,10,36);
+    /* background: RGB(48,10,36); */
     color: white;
-    height:50vh;
-    width: 50vw;
+    height:90vh;
+    width: 90vw;
     z-index: 100;
-    border-radius: 10px 10px 0 0;
+    /* border-radius: 10px 10px 0 0; */
   }
   .svgTop{
     height: 40px;
@@ -127,13 +158,15 @@ export default {
   .noteContent{
     padding: 5px 10px 5px 10px;
     flex-grow: 10;
+    /* background: RGB(48,10,36); */
   }
-  @media (max-width: 1000px) {
-    .page{
-      height: 90vh;
-      width: 90vw;
-      top: 65px;
-    }
+  .noteFooter{
+    background: #292825;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 30px;
+    padding: 0 10px;
   }
   .formField{
     display: flex;
@@ -154,8 +187,47 @@ export default {
     color: white;
     cursor: pointer;
   }
+  .page1{
+    display: flex;
+    flex-direction: column;
+    height: 40vh;
+    width: 45vw;
+    background: RGB(48,10,36);
+    border-radius: 10px 10px 0 0;
 
-  /* mobile */
+  }
+  .page2{
+    display: flex;
+    flex-direction: column;
+    height: 50vh;
+    width: 30vw;
+    background: RGB(0,27,51);
+    border-radius: 10px 10px 0 0;
+    margin-top: 200px;
+    /* border: solid .5px black; */
+  }
+
+  /* mode tablette ou petit pc */
+  /* breakpoint pour le terminal et les notes presque plein écran */
+  @media (max-width: 1000px) {
+    .page{
+      flex-direction: column;
+      height: 90vh;
+      width: 90vw;
+      top: 65px;
+    }
+    .page1{
+      height: 40vh;
+      width: 90vw;
+    }
+    .page2{
+      height: 50vh;
+      width: 90vw;
+      margin-top: 5px;
+    }
+  }
+
+  /* breakpoint pour le champs dans le terminal*/
   @media (max-width: 460px) {
     .formField{
       flex-direction: column;
@@ -165,17 +237,29 @@ export default {
     }
   }
 
-    /* mobile */
+  /* mobile */
   @media (max-width: 550px) {
     .page{
       border-radius: 0 0 0 0;
       height:100vh;
       width: 100vw;
       top: 0px;
-
+      margin-left: auto;
+      margin-right: auto;
+      left: 0px;
+      right: 0px;
     }
     .noteHeader{
       border-radius: 0 0 0 0;
+    }
+    .page1{
+      height: 50vh;
+      width: 100vw;
+    }
+    .page2{
+      height: 50vh;
+      width: 100vw;
+      margin-top: 0px;
     }
   }
 </style>
