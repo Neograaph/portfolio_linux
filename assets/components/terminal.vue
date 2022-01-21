744ad382c8037d52474fdefb0085782376f97274
Maxime Gauthier <max45270@gmail.com>
2022-01-21 17:45:31 +0100

    fix erreur console2

--- assets/components/terminal.vue
+++ assets/components/terminal.vue
@@ -7,7 +7,7 @@
   <div v-if='pageOpen' class="page">
 
     <!-- popup terminal -->
-    <div class="page1" id="page1" @click="getFocus()">  
+    <div class="page1" id="page1">  
       <div class="noteHeader">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 svgTop" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
@@ -17,7 +17,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
       </div>
-      <div class="noteContent">
+      <div class="noteContent" @click="getFocus()">
         <form class="formField" action="javascript:void(0)" @submit="execTerminalCmd">
         maxime@maxime-HM863BX ~ $
         <input v-model="cmd" class="inputField" id="inputField" type="text" :placeholder="placeholderI">
