197a8e25bbacb0e99ee003ec15946c7644ace2cd
Maxime Gauthier <max45270@gmail.com>
2022-01-24 15:21:21 +0100

    csrf token error

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -4,19 +4,32 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
-            ->add('password', PasswordType::class)
-            ->add('confirm_password', PasswordType::class)
+            ->add('username', TextType::class, [
+                'attr' => ['autofocus'=>true, 'placeholder' => 'Pseudo', 'class' => 'fieldFormLogin'],
+                'label' => FALSE
+            ])
+            ->add('email', TextType::class, [
+                'attr' => ['placeholder' => 'email', 'class' => 'fieldFormLogin'],
+                'label' => FALSE
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => ['placeholder' => 'mot de passe', 'class' => 'fieldFormLogin'],
+                'label' => FALSE
+            ])
+            ->add('confirm_password', PasswordType::class, [
+                'attr' => ['placeholder' => 'confirmer le mot de passe', 'class' => 'fieldFormLogin'],
+                'label' => FALSE
+            ])
         ;
     }
 
